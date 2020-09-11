d4b506aea75ea3428bce8f3b65c12e95078ebef1
pineapple34 <pineapple6934@gmail.com>
2020-09-11 11:53:12 +0500

    Initial commit

---
+++ Grid1/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Grid1
+{
+    partial class Grid1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.SizeTB = new System.Windows.Forms.TextBox();
+            this.BuildBtn = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(74, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Размер";
+            // 
+            // SizeTB
+            // 
+            this.SizeTB.Location = new System.Drawing.Point(92, 22);
+            this.SizeTB.Name = "SizeTB";
+            this.SizeTB.Size = new System.Drawing.Size(100, 20);
+            this.SizeTB.TabIndex = 2;
+            // 
+            // BuildBtn
+            // 
+            this.BuildBtn.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.BuildBtn.Location = new System.Drawing.Point(209, 15);
+            this.BuildBtn.Name = "BuildBtn";
+            this.BuildBtn.Size = new System.Drawing.Size(119, 30);
+            this.BuildBtn.TabIndex = 3;
+            this.BuildBtn.Text = "Построить";
+            this.BuildBtn.UseVisualStyleBackColor = true;
+            this.BuildBtn.Click += new System.EventHandler(this.BuildBtn_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.AllowUserToAddRows = false;
+            this.dataGridView1.AllowUserToDeleteRows = false;
+            this.dataGridView1.AllowUserToResizeColumns = false;
+            this.dataGridView1.AllowUserToResizeRows = false;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.ColumnHeadersVisible = false;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 51);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.ReadOnly = true;
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
+            this.dataGridView1.ShowEditingIcon = false;
+            this.dataGridView1.Size = new System.Drawing.Size(316, 231);
+            this.dataGridView1.TabIndex = 4;
+            this.dataGridView1.Visible = false;
+            // 
+            // Grid1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(340, 294);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.BuildBtn);
+            this.Controls.Add(this.SizeTB);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "Grid1";
+            this.Text = "Grid1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox SizeTB;
+        private System.Windows.Forms.Button BuildBtn;
+        private System.Windows.Forms.DataGridView dataGridView1;
+    }
+}
+
---
+++ Grid1/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Grid1
+{
+    public partial class Grid1 : Form
+    {
+        public Grid1()
+        {
+            InitializeComponent();
+        }
+
+        private void BuildBtn_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(SizeTB.Text, out int n) && n >= 1)
+            {
+                dataGridView1.Visible = true;
+
+                dataGridView1.ColumnCount = dataGridView1.RowCount = n;
+                for (int i = 0; i < n; i++)
+                    for (int k = 0; k < n; k++)
+                        dataGridView1[i, k].Value = (i == k ? 1 : 0);
+            }
+            else MessageBox.Show("Ошибка входных данных");
+        }
+    }
+}
